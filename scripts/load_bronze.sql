1813e9af93b146aa7ce6e1d46f486a1438d90f4e
karo23361 <118350099+karo23361@users.noreply.github.com>
2025-04-10 18:31:31 +0200

    Update load_bronze.sql

--- scripts/load_bronze.sql
+++ scripts/load_bronze.sql
@@ -10,7 +10,8 @@ TRUNCATE TABLE customers;
 
 LOAD DATA LOCAL INFILE '/home/pi/customers-100000.csv'
 INTO TABLE customers
-FIELDS TERMINATED BY ','  
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
 LINES TERMINATED BY '\n'  
 IGNORE 1 LINES 
 (id,customer_id, first_name, last_name, company, city, country, phone1, phone2, email, subscription_date, website);
@@ -19,7 +20,7 @@ TRUNCATE TABLE people;
 
 LOAD DATA LOCAL INFILE '/home/pi/people-100000.csv'
 INTO TABLE people
-FIELDS TERMINATED BY ',' 
+FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
 (id,user_id, first_name, last_name, sex, email, phone, date_of_birth, job_title);
@@ -28,7 +29,7 @@ TRUNCATE TABLE organizations;
 
 LOAD DATA LOCAL INFILE '/home/pi/organizations-100000.csv'
 INTO TABLE organizations
-FIELDS TERMINATED BY ',' 
+FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
 (id,organization_id, name, website, country, description, founded, industry, number_of_employees);
@@ -37,7 +38,8 @@ TRUNCATE TABLE leads;
 
 LOAD DATA LOCAL INFILE '/home/pi/leads-100000.csv'
 INTO TABLE leads
-FIELDS TERMINATED BY ',' 
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
 (id,account_id, lead_owner, first_name, last_name, company, phone1, phone2, email1, email2, website, source, deal_stage, notes);
@@ -46,7 +48,8 @@ TRUNCATE TABLE products;
 
 LOAD DATA LOCAL INFILE '/home/pi/products-100000.csv'
 INTO TABLE products
-FIELDS TERMINATED BY ',' 
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
 (id,name, description, brand, category, price, currency, stock, ean, color, size, availability, internal_id);
