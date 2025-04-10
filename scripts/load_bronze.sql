b28ec06fa3bd869f3744bd6a590974749c5e176c
karo23361 <118350099+karo23361@users.noreply.github.com>
2025-04-10 12:15:17 +0200

    Create load_bronze.sql

---
+++ scripts/load_bronze.sql
@@ -0,0 +1,54 @@
+-----------------------------------------------------
+-- Script is used for loading data into bronze layer
+
+-- > Before each load truncate table
+----------------------------------------------------- 
+
+
+
+TRUNCATE TABLE customers;
+
+LOAD DATA INFILE '/home/customers.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ','  
+LINES TERMINATED BY '\n'  
+IGNORE 1 LINES 
+(customer_id, first_name, last_name, company, city, country, phone1, phone2, email, subscription_date, website);
+
+TRUNCATE TABLE people;
+
+LOAD DATA INFILE '/home/people.csv'
+INTO TABLE people
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES
+(user_id, first_name, last_name, sex, email, phone, date_of_birth, job_title);
+
+TRUNCATE TABLE organizations;
+
+LOAD DATA INFILE '/home/organizations.csv'
+INTO TABLE organizations
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES
+(organization_id, name, website, country, description, founded, industry, number_of_employees);
+
+TRUNCATE TABLE leads;
+
+LOAD DATA INFILE '/home/leads.csv'
+INTO TABLE leads
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES
+(account_id, lead_owner, first_name, last_name, company, phone1, phone2, email1, email2, website, source, deal_stage, notes);
+
+TRUNCATE TABLE products;
+
+LOAD DATA INFILE '/home/products.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES
+(name, description, brand, category, price, currency, stock, ean, color, size, availability, internal_id);
+
+
